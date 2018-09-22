bde82fa02d043eaa51b43ec77934cf26fbf067d0
tqhoa <webnambo@gmail.com>
2018-09-22 13:29:38 +0700

    ok

---
+++ modules/base/src/Tests/Config/CanPublishConfigurationTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Inspire\Base\Tests\Config;
+
+use Inspire\Base\Tests\BaseTestCase;
+
+class CanPublishConfigurationTest extends BaseTestCase
+{
+
+    public function testExampleTest()
+    {
+        $stack = [];
+        $this->assertEmpty($stack);
+    }
+}
