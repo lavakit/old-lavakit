f2654ea320c51f50e8be272b79b9d61ce8b64429
tqhoa <webnambo@gmail.com>
2018-10-21 23:04:01 +0700

    create gulpfile.js

---
+++ Themes/backend/adminlte/gulpfile.js
@@ -0,0 +1,54 @@
+process.env.DISABLE_NOTIFIER = true;
+
+const elixir = require('laravel-elixir');
+const gulp = require('gulp');
+
+const dir_destination = './../../../public/themes/backend/adminlte/assets';
+
+/*Load file scss*/
+let filesStylesheetModules = [
+    {module:'post', file: 'post'},
+];
+
+/*Load file Js scss*/
+let filesJsModules = [
+    {module:'post', file: 'post'},
+];
+
+elixir.config.sourcemaps = false;
+elixir.inProduction = false;
+
+elixir(function (mix) {
+
+    mix.sass('./assets/sass/style.scss', 'assets/css/style.css');
+
+    /*StyleSheet in the module */
+    filesStylesheetModules.forEach(function (data) {
+        mix.sass('./../../../modules/'+ data.module +'/resources/assets/sass/'+ data.file +'.scss', 'assets/css/'+  data.file +'.css');
+    });
+
+    elixir(function(mix) {
+        mix.styles([
+            './assets/css/style.css',
+            './assets/css/post.css',
+        ], dir_destination + '/css/all.css');
+    });
+
+    mix.scripts([
+        './../../../modules/base/resources/assets/js/variable.js',
+        './../../../modules/base/resources/assets/js/csrf.js',
+        './../../../modules/base/resources/assets/js/core.js'
+    ], 'assets/js/core.js')
+
+    /*Javascript in the module*/
+    filesJsModules.forEach(function (data) {
+        mix.scripts('./../../../modules/' + data.module + '/resources/assets/js/' + data.file + '.js', 'assets/js/app_modules/' + data.file + '.js');
+        mix.scripts('./../../../modules/' + data.module + '/resources/assets/js/' + data.file + '.js', dir_destination + '/js/app_modules/' + data.file + '.js');
+    });
+
+    /*Copy an entire Images directory*/
+    mix.copy('assets/images', '../../../public/themes/backend/adminlte/assets/images');
+
+    /*Copy an entire vendor directory*/
+    mix.copy('assets/vendor', '../../../public/themes/backend/adminlte/assets/vendor');
+});
