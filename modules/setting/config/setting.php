1b9f209cf30ebea103cc1bc141d8510d4fa7a81e
hoatq <hoatq@lucy.ne.jp>
2019-02-18 22:30:32 +0700

    create page setting email

--- modules/setting/config/setting.php
+++ modules/setting/config/setting.php
@@ -5,7 +5,20 @@ return [
 
     ],
     'emails' => [
-
+        [
+            'package' => 'Main',
+            'module' => 'setting',
+            'control' => 'text',
+            'key' => 'smtp_from_name',
+            'name' => trans('setting::form.emails.smtp.from_email'),
+            'value' => '',
+            'options' => [
+                'class' => 'form-control',
+                'placeholder' => 'info@example.info',
+                'description' => trans('setting::form.email.smtp.description.from_email')
+            ],
+            'is_admin' => false
+        ],
     ],
     'medias' => [
 
