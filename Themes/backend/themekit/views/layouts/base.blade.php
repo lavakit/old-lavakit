d08dfe209628d3c8ac90c830bb0187d5cc7e97c9
tqhoa <webnambo@gmail.com>
2019-03-26 21:55:59 +0700

    add images

--- Themes/backend/themekit/views/layouts/base.blade.php
+++ Themes/backend/themekit/views/layouts/base.blade.php
@@ -10,7 +10,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <link rel="icon" href="favicon.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="{{ backendAsset('images/favicon/favicon.ico') }}" type="image/x-icon" />
+        <link rel="icon" href="{{ backendAsset('images/favicon/favicon.ico') }}" type="image/x-icon" />
+        <link rel="shortcut icon" href="{{ backendAsset('images/favicon/favicon.ico') }}"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ backendAsset('images/favicon/favicon.png') }}">
 
         <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700,800" rel="stylesheet">
         {{-- Styles --}}
