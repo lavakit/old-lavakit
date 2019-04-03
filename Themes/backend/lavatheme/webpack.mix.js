3c77a89da5bcc6233e0cc98e418211a5ca629668
tqhoa <webnambo@gmail.com>
2019-04-03 22:44:25 +0700

    make basic yarn

---
+++ Themes/backend/lavatheme/webpack.mix.js
@@ -0,0 +1,24 @@
+let mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+const dir_destination = './../../../public/themes/backend/lavatheme/assets';
+
+mix.setPublicPath('./../../../public');
+
+mix.js('assets/js/lavatheme.js', 'themes/backend/lavatheme/assets/js/lavatheme.js').extract(['jquery', 'vue'])
+    .sass('./assets/sass/style.scss', 'themes/backend/lavatheme/assets/css/style.css').options({
+    processCssUrls: false,
+});
+
+/** Copy vendor */
+mix.copy('node_modules/icon-kit/dist/fonts', dir_destination + '/fonts')
