ef45252017bc33df9d70342df389905cde3d2161
tqhoa <webnambo@gmail.com>
2019-04-08 16:18:20 +0700

    load dymanic language

--- modules/base/config/cache.php
+++ modules/base/config/cache.php
@@ -3,5 +3,13 @@
 return [
     'cache_enable'  =>  env('CACHE_ENABLE', false),
     'cache_time'    =>  10,
-    'cache_store'   =>  storage_path() . '/framework/cache/cache_keys.json'
+    'cache_store'   =>  storage_path() . '/framework/cache/cache_keys.json',
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Here you may specify the number of minutes that you wish
+    | the Cache tags remember
+    |--------------------------------------------------------------------------
+    */
+    'cache_remember_time' => env('CACHE_REMEMBER_TIME', 120),
 ];
