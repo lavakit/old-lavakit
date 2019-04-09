54505d033f18a414eab357a6ceec1a47a06eecc4
tqhoa <webnambo@gmail.com>
2019-04-09 14:36:28 +0700

    Finish

--- Themes/backend/lavatheme/resources/src/js/components/Sidebar.vue
+++ Themes/backend/lavatheme/resources/src/js/components/Sidebar.vue
@@ -57,119 +57,122 @@
                         ]
                     },
 
-                    {
-                        name: 'Content',
-                        menuParent: [
-                            {
-                                uri: '#',
-                                icon: 'ik-box',
-                                name: 'Blog',
-                                active: false,
-                                hasSub: true,
-                                menuChild: [
-                                    {uri: '#', name: 'Pages'},
-                                    {uri: '#', name: 'Posts'},
-                                    {uri: '#', name: 'Products'}
-                                ]
-                            },
-                            {
-                                uri: '#',
-                                icon: 'ik-gitlab',
-                                name: 'Menu',
-                                active: false,
-                                hasSub: true,
-                                menuChild: [
-                                    {uri: '#', name: 'Categories'},
-                                    {uri: '#', name: 'Tags'}
-                                ]
-                            },
-                            {
-                                uri: '#',
-                                icon: 'ik-gitlab',
-                                name: 'Media',
-                                active: false,
-                                hasSub: true,
-                                menuChild: [
-                                    {uri: '#', name: 'Library'},
-                                    {uri: '#', name: 'Galleries'}
-                                ]
-                            },
-                        ]
-                    },
-                    {
-                        name: 'Appearance',
-                        menuParent: [
-                            {uri: '#', icon: 'ik-edit', name: 'Themes', active: false, hasSub: false},
-                            {uri: '#', icon: 'ik-edit', name: 'Customize', active: false, hasSub: false},
-                            {uri: '#', icon: 'ik-menu', name: 'Navigation', active: false, hasSub: false},
-                            {uri: '#', icon: 'ik-menu', name: 'Navigation', active: false, hasSub: false, number: 3},
-                            {
-                                uri: '#',
-                                icon: 'ik-gitlab',
-                                name: 'Media',
-                                active: false,
-                                hasSub: true,
-                                number: 5,
-                                menuChild: [
-                                    {uri: '#', name: 'Installed plugins'},
-                                    {uri: '#', name: 'Add new'}
-                                ]
-                            },
+                    // {
+                    //     name: 'Content',
+                    //     menuParent: [
+                    //         {
+                    //             uri: '#',
+                    //             icon: 'ik-box',
+                    //             name: 'Blog',
+                    //             active: false,
+                    //             hasSub: true,
+                    //             menuChild: [
+                    //                 {uri: '#', name: 'Pages'},
+                    //                 {uri: '#', name: 'Posts'},
+                    //                 {uri: '#', name: 'Products'}
+                    //             ]
+                    //         },
+                    //         {
+                    //             uri: '#',
+                    //             icon: 'ik-gitlab',
+                    //             name: 'Menu',
+                    //             active: false,
+                    //             hasSub: true,
+                    //             menuChild: [
+                    //                 {uri: '#', name: 'Categories'},
+                    //                 {uri: '#', name: 'Tags'}
+                    //             ]
+                    //         },
+                    //         {
+                    //             uri: '#',
+                    //             icon: 'ik-gitlab',
+                    //             name: 'Media',
+                    //             active: false,
+                    //             hasSub: true,
+                    //             menuChild: [
+                    //                 {uri: '#', name: 'Library'},
+                    //                 {uri: '#', name: 'Galleries'}
+                    //             ]
+                    //         },
+                    //     ]
+                    // },
 
-                        ]
-                    },
+                    // {
+                    //     name: 'Appearance',
+                    //     menuParent: [
+                    //         {uri: '#', icon: 'ik-edit', name: 'Themes', active: false, hasSub: false},
+                    //         {uri: '#', icon: 'ik-edit', name: 'Customize', active: false, hasSub: false},
+                    //         {uri: '#', icon: 'ik-menu', name: 'Navigation', active: false, hasSub: false},
+                    //         {uri: '#', icon: 'ik-menu', name: 'Navigation', active: false, hasSub: false, number: 3},
+                    //         {
+                    //             uri: '#',
+                    //             icon: 'ik-gitlab',
+                    //             name: 'Media',
+                    //             active: false,
+                    //             hasSub: true,
+                    //             number: 5,
+                    //             menuChild: [
+                    //                 {uri: '#', name: 'Installed plugins'},
+                    //                 {uri: '#', name: 'Add new'}
+                    //             ]
+                    //         },
+                    //
+                    //     ]
+                    // },
+
                     {
                         name: 'Systems',
                         menuParent: [
                             {
-                                uri: '#',
+                                uri: false,
                                 icon: 'ik-settings',
                                 name: 'Settings',
                                 active: false,
                                 hasSub: true,
                                 menuChild: [
-                                    {uri: '/admin/settings/general', name: 'General'},
+                                    //{uri: '/admin/settings/general', name: 'General'},
                                     {uri: 'admin.settings.email', name: 'Email'},
-                                    {uri: '/admin/settings/email', name: 'Media'}
+                                    //{uri: '/admin/settings/media', name: 'Media'}
                                 ]
                             },
 
-                            {
-                                uri: '#',
-                                icon: 'ik-edit',
-                                name: 'Accounts',
-                                active: false,
-                                hasSub: true,
-                                menuChild: [
-                                    {uri: '#', name: 'Profile'},
-                                    {uri: '#', name: 'User'},
-                                    {uri: '#', name: 'Permission'},
-                                    {uri: '#', name: 'Role'},
-                                    {uri: '#', name: 'API keys'}
-                                ]
-                            },
-                            {
-                                uri: '#',
-                                icon: 'ik-menu',
-                                name: 'Navigation',
-                                active: false,
-                                number: 4,
-                                hasSub: true,
-                                menuChild: [
-                                    {uri: '#', name: 'Import'},
-                                    {uri: '#', name: 'Export'},
-                                ]
-
-                            },
+                            // {
+                            //     uri: '#',
+                            //     icon: 'ik-edit',
+                            //     name: 'Accounts',
+                            //     active: false,
+                            //     hasSub: true,
+                            //     menuChild: [
+                            //         {uri: '#', name: 'Profile'},
+                            //         {uri: '#', name: 'User'},
+                            //         {uri: '#', name: 'Permission'},
+                            //         {uri: '#', name: 'Role'},
+                            //         {uri: '#', name: 'API keys'}
+                            //     ]
+                            // },
+                            // {
+                            //     uri: '#',
+                            //     icon: 'ik-menu',
+                            //     name: 'Navigation',
+                            //     active: false,
+                            //     number: 4,
+                            //     hasSub: true,
+                            //     menuChild: [
+                            //         {uri: '#', name: 'Import'},
+                            //         {uri: '#', name: 'Export'},
+                            //     ]
+                            //
+                            // },
                         ]
                     },
-                    {
-                        name: 'Support',
-                        menuParent: [
-                            {uri: '#', icon: 'ik-monitor', name: 'Documentation', active: false, hasSub: false},
-                            {uri: '#', icon: 'ik-help-circle', name: 'Donate', active: false, hasSub: false},
-                        ]
-                    }
+
+                    // {
+                    //     name: 'Support',
+                    //     menuParent: [
+                    //         {uri: '#', icon: 'ik-monitor', name: 'Documentation', active: false, hasSub: false},
+                    //         {uri: '#', icon: 'ik-help-circle', name: 'Donate', active: false, hasSub: false},
+                    //     ]
+                    // }
                 ]
             }
         }
